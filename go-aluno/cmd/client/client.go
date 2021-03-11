674299d42198aa7eefb9b5bf95e4b901cd670efe
Felipe Carvalho <felipee_sc@hotmail.com>
2021-03-10 22:46:06 -0300

    implement server stream

--- go-aluno/cmd/client/client.go
+++ go-aluno/cmd/client/client.go
@@ -1,1 +1,35 @@
-package client
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+)
+
+func main() {
+	connection, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("não conseguiu conectar com gRPC server %v", err)
+	}
+	defer connection.Close()
+
+	client := pb.NewUserServiceClient(connection)
+}
+
+func AddUser(client pb.UserServiceClient) {
+
+	req := &pb.User{
+		Id: "0",
+		Name: "Carvalho",
+		Email: "felipe@carvalho.com",
+	}
+
+	res, err := client.AddUser(context.Background(), req)
+	if err != nil {
+		log.Fatalf("não conseguiu fazer requisição com gRPC %v", err)
+	}
+
+	fmt.Println(res)
+
+//	TODO   go run cmd/client/client.go
+}
--- services/user.go
+++ services/user.go
@@ -3,11 +3,13 @@ package services
 import (
 	"context"
 	"github.com/felipeesc/grpc/pb"
+	"time"
 )
 
 type UserServiceServer interface {
 	AddUser(context.Context, *User) (*User, error)
 	mustEmbedUnimplementedUserServiceServer()
+	AddUserVerbose(ctx context.Context, in *User, opts ...grpc.CallOption) (UserService_AddUserVerboseClient, error)
 }
 
 type UserService struct {
@@ -26,6 +28,44 @@ func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error)
 		Name: req.GetName(),
 		Email: req.GetEmail(),
 	}, nil
+}
+
+func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVerboseServer) error {
+
+	stream.Send(*pb.UserResultStream{
+		Status: "Iniciando",
+		User: &pb.User{},
+	})
+	time.Sleep(time.Second * 3)
+
+	stream.Send(*pb.UserResultStream{
+		Status: "Inserindo",
+		User: &pb.User{},
+	})
+	time.Sleep(time.Second * 3)
+
+	stream.Send(*pb.UserResultStream{
+		Status: "Usuário inserindo",
+		User: &pb.User{
+			Id: "123",
+			Name: req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+	time.Sleep(time.Second * 3)
+
+	stream.Send(*pb.UserResultStream{
+		Status: "Completo",
+		User: &pb.User{
+			Id: "123",
+			Name: req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	return nil
 }
 
 // instala o evans .. para fazer primeira chamada...
